7d1803e9883ec59ea5eb0ad0e35bd754541d1787
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2024-05-02 02:10:58 +0530

    added stripe gateway

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,5 @@
-import Stripe from "stripe";
-
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2022-11-15",
-  typescript: true,
-});
+  import Stripe from "stripe";
+  export const stripe = new Stripe('sk_test_51PBi4DSCQ3uESQFzKDlHrwO89eHkfXo2lFwdcCJzA3RzBF0SZMIboVSaAuBJ18O8HkjVt9MxWsytPQfKHoyysOmQ00cn1sz2L3', {
+    apiVersion: "2022-11-15",
+    typescript: true,
+  });
